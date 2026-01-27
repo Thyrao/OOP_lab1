0953bf43adf2640ac185ab249121fbf438c63874
Thyra <fredriksdottir@gmail.com>
2026-01-27 12:55:33 +0100

    alla tester puchas inte?

--- Lab1/test/Saab95Test.java
+++ Lab1/test/Saab95Test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions;
 
 import java.awt.*;
 
-public class SaabTest {
+public class Saab95Test {
     Saab95 Saab = new Saab95();
 
     @Test
@@ -44,14 +44,14 @@ public class SaabTest {
     public void incrementSpeed() {
         double inc = Saab.getCurrentSpeed() + Saab.speedFactor() * 0.5;
         Saab.incrementSpeed(0.5);
-        Assertions.assertEquals(inc, Saab.getCurrentSpeed());
+        Assertions.assertEquals(inc, Saab.getCurrentSpeed(), "Expected increased speed");
     }
 
     @Test
     public void decrementSpeed() {
         double dec = Saab.getCurrentSpeed() - Saab.speedFactor() * 0.5;
         Saab.decrementSpeed(0.5);
-        Assertions.assertEquals(dec, Saab.getCurrentSpeed());
+        Assertions.assertEquals(dec, Saab.getCurrentSpeed(), "The speed should've decreased");
     }
 
     @Test
@@ -60,16 +60,36 @@ public class SaabTest {
         Saab.gas(0.5);
         double newSpeed = Saab.getCurrentSpeed();
         boolean greaterThen = newSpeed > oldSpeed;
-        Assertions.assertTrue(greaterThen);
+        Assertions.assertTrue(greaterThen, "The new speed should be bigger than the old speed");
+    }
+
+    @Test
+    public void gas_on_max() {
+        while (Saab.getCurrentSpeed() < Saab.getEnginePower()){
+            Saab.gas(1);
+        }
+        Saab.gas(0.5);
+        double newSpeed = Saab.getCurrentSpeed();
+        Assertions.assertEquals(newSpeed, Saab.getEnginePower());
+
+    }
+
+    @Test
+    public void brakeOn0() {
+        Saab.gas(0.3);
+        Saab.stopEngine();
+        Saab.brake(0.1);
+        Assertions.assertEquals(0, Saab.getCurrentSpeed(), "Break with no speed");
     }
 
     @Test
     public void brake() {
+        Saab.gas(0.3);
         double oldSpeed = Saab.getCurrentSpeed();
-        Saab.brake(0.5);
+        Saab.brake(0.1);
         double newSpeed = Saab.getCurrentSpeed();
         boolean greaterThen = newSpeed < oldSpeed;
-        Assertions.assertTrue(greaterThen);
+        Assertions.assertTrue(greaterThen, "The new speed should be less then the old speed");
     }
 
     @Test
@@ -77,15 +97,15 @@ public class SaabTest {
         double oldDirection = Saab.getDirection();
         Saab.turnRight();
         double exp = (oldDirection + 1)%4;
-        Assertions.assertEquals(exp, Saab.getDirection());
+        Assertions.assertEquals(exp, Saab.getDirection(), "The direction didn't update correctly when turning right");
     }
 
     @Test
     public void turnLeft() {
         double oldDirection = Saab.getDirection();
         Saab.turnLeft();
-        double exp = ((oldDirection - 1)%4 +4) % 4; // kan inte ha modulo på ett negativt tal
-        Assertions.assertEquals(exp, Saab.getDirection());
+        double exp = ((oldDirection - 1)%4 +4) % 4;
+        Assertions.assertEquals(exp, Saab.getDirection(), "The direction didn't update correctly when turning left");
     }
 
     /*@Test
@@ -97,11 +117,44 @@ public class SaabTest {
     }*/
 
     @Test
-    public void move() {
+    public void move0() {
         double[] beforePos = Saab.getPosition();
         Saab.gas(0.5);
         Saab.move();
         double[] afterPos = Saab.getPosition();
-        Assertions.assertNotEquals(beforePos, afterPos);
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
+
+    @Test
+    public void move1() {
+        double[] beforePos = Saab.getPosition();
+        Saab.gas(0.5);
+        Saab.turnRight();
+        Saab.move();
+        double[] afterPos = Saab.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
+
+    @Test
+    public void move2() {
+        double[] beforePos = Saab.getPosition();
+        Saab.gas(0.5);
+        Saab.turnRight();
+        Saab.turnRight();
+        Saab.move();
+        double[] afterPos = Saab.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
+
+    @Test
+    public void move3() {
+        double[] beforePos = Saab.getPosition();
+        Saab.gas(0.5);
+        Saab.turnRight();
+        Saab.turnRight();
+        Saab.turnRight();
+        Saab.move();
+        double[] afterPos = Saab.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
     }
 }
--- Lab1/test/Volvo240Test.java
+++ Lab1/test/Volvo240Test.java
@@ -1,4 +1,158 @@
-import static org.junit.jupiter.api.Assertions.*;
-class Volvo240Test {
-  
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.awt.*;
+
+public class Volvo240Test {
+    Volvo240 Volvo = new Volvo240();
+
+    @Test
+    public void getNrDoors() {
+        Assertions.assertEquals(4, Volvo.getNrDoors(), "Number of doors should be 4");
+    }
+
+    @Test
+    public void getEnginePower() {Assertions.assertEquals(100, Volvo.getEnginePower(), "Engine power should be 100");}
+
+    @Test
+    public void get_set_Color() {
+        Volvo.setColor(Color.black);
+        Assertions.assertEquals(Color.black, Volvo.getColor(), "Every cool car is Black...");
+    }
+
+    @Test
+    public void startEngine() {
+        Volvo.startEngine();
+        Assertions.assertEquals(0.1,Volvo.getCurrentSpeed(), "When engine has started the speed should be 0.1");
+    }
+
+    @Test
+    public void stopEngine() {
+        Volvo.stopEngine();
+        Assertions.assertEquals(0,Volvo.getCurrentSpeed(), "When engine is stopped it should be 0");
+    }
+
+    @Test
+    public void speedFactor() {
+        Assertions.assertEquals(1.25, Volvo.speedFactor(), "Expected speed factor");
+    }
+
+    @Test
+    public void incrementSpeed() {
+        double inc = Math.min(Volvo.getCurrentSpeed() + Volvo.speedFactor() * 0.5, Volvo.getEnginePower());
+        Volvo.incrementSpeed(0.5);
+        Assertions.assertEquals(inc, Volvo.getCurrentSpeed(), "Expected increased speed");
+    }
+
+    @Test
+    public void decrementSpeed() {
+        double dec = Math.max(Volvo.getCurrentSpeed() - Volvo.speedFactor() * 0.5, 0);
+        Volvo.decrementSpeed(0.5);
+        Assertions.assertEquals(dec, Volvo.getCurrentSpeed(), "The speed should've decreased");
+    }
+
+    @Test
+    public void gas() {
+        double oldSpeed = Volvo.getCurrentSpeed();
+        Volvo.gas(0.5);
+        double newSpeed = Volvo.getCurrentSpeed();
+        boolean greaterThen = newSpeed > oldSpeed;
+        Assertions.assertTrue(greaterThen, "The new speed should be bigger than the old speed");
+    }
+
+    @Test
+    public void gas_on_max() {
+        while ( Volvo.getCurrentSpeed() < Volvo.getEnginePower()){
+            Volvo.gas(1);
+        }
+        Volvo.gas(0.5);
+        double newSpeed = Volvo.getCurrentSpeed();
+        Assertions.assertEquals(newSpeed, Volvo.getEnginePower());
+
+    }
+
+    @Test
+    public void brakeOn0() {
+        Volvo.gas(0.3);
+        Volvo.stopEngine();
+        Volvo.brake(0.1);
+        Assertions.assertEquals(0, Volvo.getCurrentSpeed(), "Break with no speed");
+    }
+
+    @Test
+    public void brake() {
+        Volvo.gas(0.3);
+        double oldSpeed = Volvo.getCurrentSpeed();
+        Volvo.brake(0.1);
+        double newSpeed = Volvo.getCurrentSpeed();
+        boolean greaterThen = newSpeed < oldSpeed;
+        Assertions.assertTrue(greaterThen, "The new speed should be less then the old speed");
+    }
+
+
+    @Test
+    public void turnRight() {
+        double oldDirection = Volvo.getDirection();
+        Volvo.turnRight();
+        double exp = (oldDirection + 1)%4;
+        Assertions.assertEquals(exp, Volvo.getDirection(), "The new direction after turning right");
+    }
+
+    @Test
+    public void turnLeft() {
+        double oldDirection = Volvo.getDirection();
+        Volvo.turnLeft();
+        double exp = ((oldDirection - 1)%4 +4) % 4; // kan inte ha modulo på ett negativt tal
+        Assertions.assertEquals(exp, Volvo.getDirection(), "The new direction after turning left");
+    }
+
+    /*@Test
+    public void getPosition() {
+        double posX = Saab.getPosition()[0];
+        double posY = Saab.getPosition()[1];
+        Saab.move()
+        Assertions.assertEquals(0.0, Saab.getPosition()[0]);
+    }*/
+
+    @Test
+    public void move0() {
+        double[] beforePos = Volvo.getPosition();
+        Volvo.gas(0.5);
+        Volvo.move();
+        double[] afterPos = Volvo.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
+
+    @Test
+    public void move1() {
+        double[] beforePos = Volvo.getPosition();
+        Volvo.gas(0.5);
+        Volvo.turnRight();
+        Volvo.move();
+        double[] afterPos = Volvo.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
+
+    @Test
+    public void move2() {
+        double[] beforePos = Volvo.getPosition();
+        Volvo.gas(0.5);
+        Volvo.turnRight();
+        Volvo.turnRight();
+        Volvo.move();
+        double[] afterPos = Volvo.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
+
+    @Test
+    public void move3() {
+        double[] beforePos = Volvo.getPosition();
+        Volvo.gas(0.5);
+        Volvo.turnRight();
+        Volvo.turnRight();
+        Volvo.turnRight();
+        Volvo.move();
+        double[] afterPos = Volvo.getPosition();
+        Assertions.assertNotEquals(beforePos, afterPos, "The position should have changed if current speed > 0");
+    }
 }
